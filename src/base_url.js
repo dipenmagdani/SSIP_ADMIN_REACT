41a985fea0d54c4956363236c21dcd1406c9f614
aman bhatt <22csama003@gmail.com>
2023-11-22 21:59:13 +0530

    student reg form

--- src/base_url.js
+++ src/base_url.js
@@ -1,4 +1,4 @@
-const base_url= "https://4556-2405-201-2024-b862-dcec-69bc-5090-a563.ngrok-free.app";
+const base_url= "https://425b-2405-201-2024-b862-d79a-575-3f0a-75dd.ngrok-free.app";
 // const token = localStorage.getItem('accessToken')
 // const header = {
 //     "Content-Type":"application/json",
