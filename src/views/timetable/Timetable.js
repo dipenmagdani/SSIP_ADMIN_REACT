a9897e238f158ec101c74ea2696ea2b2caeade77
AMAN-B-H-A-T-T <22csama003@ldce.ac.in>
2024-02-08 21:53:29 +0530

    timetable

--- src/views/timetable/Timetable.js
+++ src/views/timetable/Timetable.js
@@ -125,7 +125,6 @@ const Timetable = () => {
           <CCard>
             <CCardHeader>Semester</CCardHeader>
             <CCardBody>
-              
               <CFormSelect
                 aria-label="Default select example"
                 onChange={(e) => {
@@ -151,26 +150,22 @@ const Timetable = () => {
               <CRow className='flex-column'style={{padding:"0"}}> 
               <CCol className='mb-4 d-flex align-items-center flex-column'>
                   <div className='row w-100 justify-content-between mb-2 border rounded p-2 text-dark' style={{backgroundColor:'#ebedef'}}>
-              <CCol className='mb-4 d-flex align-items-center flex-column'>
-                  <div className='row w-100 justify-content-between mb-2 border rounded p-2 text-dark' style={{backgroundColor:'#ebedef'}}>
                     <CCol>
                       <div className='w-100 text-left' >Monday</div>
                     </CCol>
                     <CCol>
                       <div className='w-100 text-end'>
                         <div className='h-20'>
-
                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/></svg>
-                          
                         </div>
                       </div>
                     </CCol>
                   </div>                  
                     <CCard className="w-100">
                       <CCardBody>
-                        <CRow className=''>
+                        <CRow>
                           <CCol sm={12} md={12} lg={12}>
-                            <CRow sm={12} md={12} lg={12} className='justify-content-center'>
+                            <CRow className='justify-content-center'>
                               <CCol sm={11} md={11} lg={12} className='mb-2'>
                                 <CCard>
                                   <CCardHeader>
@@ -220,11 +215,7 @@ const Timetable = () => {
                     <CCol>
                       <div className='w-100 text-end'>
                         <div className='h-20'>
-
                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/></svg>
-
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/></svg>
-
                         </div>
                       </div>
                     </CCol>
@@ -233,7 +224,7 @@ const Timetable = () => {
                       <CCardBody>
                         <CRow className=''>
                           <CCol sm={12} md={12} lg={12}>
-                            <CRow sm={12} md={12} lg={12} className='justify-content-center'>
+                            <CRow className='justify-content-center'>
                               <CCol sm={11} md={11} lg={12} className='mb-2'>
                                 <CCard>
                                   <CCardHeader>
