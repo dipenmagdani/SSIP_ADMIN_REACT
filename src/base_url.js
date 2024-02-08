ab35af0caf1d1df514a77cebffa7a2de87bf0846
PremSingh192 <premsingh282003@gmail.com>
2024-02-08 14:16:04 +0530

    base url

--- src/base_url.js
+++ src/base_url.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-const base_url= "https://b185-2405-201-2024-b862-f89c-da9a-4909-7913.ngrok-free.app";
-=======
 const base_url= "https://8c1e-14-139-110-146.ngrok-free.app";
 
->>>>>>> upstream/ec_implementation
 // const token = localStorage.getItem('accessToken')
 // const header = {
 //     "Content-Type":"application/json",
