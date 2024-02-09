5923dad802c0a081db121b64ba78f0f8880f3a68
Manav1011 <manavshah1011.ms@gmail.com>
2024-02-10 00:17:25 +0530

    Login form

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -1,64 +1,54 @@
 import React, { useEffect } from 'react';
 import Axios from 'axios';
-import { useNavigate , redirect } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { useContext } from 'react'
 import { Store } from 'src/views/forms/validation/store';
 import base_url from 'src/base_url';
 import "./LoginForm.css";
 import expireToken from 'src/global_function/unauthorizedToken';
-import meet_illustration from './remote-meet.png'
-import { CCol, CRow } from '@coreui/react';
-
+import '../../../css/tailwind.css'
+import { useForm } from "react-hook-form";
 
 
 export default function Login(){
       
 
      const navigate = useNavigate();
-  
-  
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-  
-     const { state, dispatch: ctxDispatch } = useContext(Store);
-     const { refreshToken , set404 } = state;
+     const { register, handleSubmit} = useForm();      
     
-  
-    const submitHandler = async (e) => {
+      const { state, dispatch: ctxDispatch } = useContext(Store);
+      const { refreshToken , set404 } = state;
 
-      e.preventDefault();
-      const header = {
-        'ngrok-skip-browser-warning':true
-      }
-      Axios.post(`${base_url}/auth/api/login/`,{
-        "email":  email,
-         "password":password        
-      },{header})
-      .then((response)=>{        
-        ctxDispatch({ type: 'ACCESS_TOKEN', payload: response.data.access});
-        ctxDispatch({ type: 'REFRESH_TOKEN', payload: response.data.refresh });
-
-        localStorage.setItem('accessToken',response.data.access)
-        localStorage.setItem('refreshToken',response.data.refresh)        
-        navigate('/')
-      })
-      .catch((error)=>{
-        console.log(error.code);
-        if(error.code === "ERR_NETWORK"){
-          ctxDispatch({ type: 'SET_404', payload: true });
+      const SubmitLogin = (data) => {
+        const header = {
+          'ngrok-skip-browser-warning':true
         }
-        else{
-          if(error.response.status === 401){
-            expireToken(refreshToken)            
-        }
-        }
-        
-      })
+        Axios.post(`${base_url}/auth/api/login/`,{
+          "email":  data.email,
+          "password":data.password        
+        },{header})
+        .then((response)=>{        
+          ctxDispatch({ type: 'ACCESS_TOKEN', payload: response.data.access});
+          ctxDispatch({ type: 'REFRESH_TOKEN', payload: response.data.refresh });
 
-    };
-  
-
+          localStorage.setItem('accessToken',response.data.access)
+          localStorage.setItem('refreshToken',response.data.refresh)        
+          navigate('/')
+        })
+        .catch((error)=>{
+          console.log(error.code);
+          if(error.code === "ERR_NETWORK"){
+            ctxDispatch({ type: 'SET_404', payload: true });
+          }
+          else{
+            if(error.response.status === 401){
+              expireToken(refreshToken)            
+          }
+          }
+          
+        })
+    }
     useEffect(()=>{      
       if(set404){
         navigate("/404")
@@ -68,26 +58,23 @@ export default function Login(){
     },[set404])    
 
   return (
-    <div className='d-flex justify-content-center align-items-center' style={{height:'100vh',flexWrap:'wrap'}}>
-    <div className="illustration-auth container" style={{flex:'1', minWidth:'300px'}}>
-      <img src={meet_illustration} alt="" style={{width:'100%',height:'auto'}}/>
-    </div>
-    <div id="login-form" style={{marginLeft:'2rem',marginRight:'2rem',flex:'1'}}>
-      <h1>Login</h1>
-      <div>
-      <CRow>
-        <CCol>Admin</CCol>
-        <CCol>Teacher</CCol>
-      </CRow>
+    <div className="h-screen bg-center bg-no-repeat sm:p-20 p-6" style={{background:'black'}}>
+      <section className="w-full h-full flex justify-center items-center rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-100" style={{backgroundImage:'url(/images/background2.jpg)'}}>
+      <div className="text-white sm:w-full">        
+        <form className="max-w-sm mx-auto" onSubmit={handleSubmit(SubmitLogin)} autoComplete='off'>
+          <div className="relative border-b-2	border-slate-500 z-0 w-full mb-5 group">
+            <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required {...register('email')}/>
+            <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+          </div>
+          <div className="relative z-0 border-b-2	border-slate-500 w-full mb-5 group">
+            <input type="password" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required  {...register('password')}/>
+            <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+          </div>
+          <button type="submit" className="w-100 focus:ring-4  font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center  text-slate-300" style={{border:'1px solid #ffa31a'}}> Submit </button>
+        </form>
+
       </div>
-      <form onSubmit={submitHandler}>
-        <label htmlFor="username">Email:</label>
-        <input type="text" id="usermail "name="email" onChange={e => setEmail(e.target.value)} placeholder="example@gmail.com"></input>
-        <label htmlFor="password">Password:</label>
-        <input type="password" onChange={e => setPassword(e.target.value)} placeholder="Password" name="password"></input>
-        <button className="form-control btn btn-outline-dark" type="submit" value="Submit">Login</button>
-      </form>
+      </section>
     </div>    
-    </div>
   );
 };
