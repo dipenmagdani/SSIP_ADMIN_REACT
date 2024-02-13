b110c9bd54adccfb408ec50d2df984668e1da792
Manav1011 <manavshah1011.ms@gmail.com>
2024-02-13 15:41:42 +0530

    Export csv feature dded

--- src/base_url.js
+++ src/base_url.js
@@ -1,8 +1,5 @@
-
 export const base_url= "http://172.16.2.98:8000";
 export const websocket = "ws://172.16.2.98:8000"
-
-
 
 // const token = localStorage.getItem('accessToken')
 // const header = {
--- src/views/teacher/Sessionmanage.js
+++ src/views/teacher/Sessionmanage.js
@@ -1,4 +1,6 @@
 import React, { useState, useRef, useEffect, useContext } from 'react'
+import { DownloadTableExcel } from 'react-export-table-to-excel';
+
 import {
   COffcanvas,
   COffcanvasBody,
@@ -29,6 +31,8 @@ const Sessionmanage = () => {
   const [attendances, setAttendances] = useState(null)
   const { state, dispatch: ctxDispatch } = useContext(Store)
   const { accessToken } = state
+  const tableRef = useRef(null);
+  const [sessionEnded,setSessionEnded] = useState(false)
   const ws = useRef(null)
   const navigate =  useNavigate()
   const [StoredTokens, CallAPI] = useAPI()
@@ -83,10 +87,12 @@ const Sessionmanage = () => {
           if (data.message.action == 'attendance_marked') {
             setAttendances((prevArray) => [...prevArray, data.message.data])
           }else if (data.message.action == 'session_ended') {
-            
+            setAttendances(data.message.data.marked_attendances)
               alert('Session has ended')
-              navigate("/teacherdashboard")
+              setSessionEnded(true)
+              // navigate("/teacherdashboard")
           }else if (data.message.action == 'session_already_ended') {
+              // setSessionEnded(true)
               alert('Session has already been ended')
           }
         }
@@ -111,6 +117,7 @@ const Sessionmanage = () => {
     <>
       {session_data ? (
         <>
+        
           <CRow>
             <CCol>
               <CCard className="card">
@@ -159,39 +166,47 @@ const Sessionmanage = () => {
             <CCol xs>
               <CCard className="mb-4">
                 <CCardHeader className="">                  
-                  <div className='my-2 w-100 btn btn btn-outline-danger' onClick={() => {stopAttendance()}}>
-                    <span className='me-3'>End Session</span>
+                 {!sessionEnded ? ( <div className='my-2 w-100 btn btn btn-outline-danger' onClick={() => {stopAttendance()}}>
+                    <span className='me-3 d-inline'>End Session</span>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="16"
                       height="16"
                       fill="currentColor"
-                      className="bi bi-stop-circle-fill"
+                      className="d-inline bi bi-stop-circle-fill"
                       viewBox="0 0 16 16"
                     >
                       <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.5 5A1.5 1.5 0 0 0 5 6.5v3A1.5 1.5 0 0 0 6.5 11h3A1.5 1.5 0 0 0 11 9.5v-3A1.5 1.5 0 0 0 9.5 5z" />
                     </svg>
-                  </div>
+                  </div>) : (<DownloadTableExcel
+                        filename={`${session_data.lecture.subject.subject_name} - ${new Date(session_data.lecture.session.day).toLocaleString()}`}
+                        sheet="users"
+                        currentTableRef={tableRef.current}
+                    >
+
+                      <button className='my-2 w-100 btn btn btn-outline-primary'> Export excel </button>
+
+                    </DownloadTableExcel>)}
+
                 </CCardHeader>
                 <CCardBody>
                   <CRow className="w-100 flex-col">
                     <CCol className="col-12">
-                      <CTable align="middle" className="mb-0 border text-center" hover responsive>
+                    <div className='table-responsive'>
+                      <table align="middle" className="table align-middle table-hover mb-0 border text-center mb-0 border text-center" hover responsive ref={tableRef}>
                         <CTableHead color="light">
-                          <CTableRow aria-colspan={4}>
-                            <CTableHeaderCell>L.D COLLEGE OF ENGINEERING</CTableHeaderCell>
+                        <CTableRow aria-colspan={5}>
+                            <CTableHeaderCell colSpan={5}>L.D COLLEGE OF ENGINEERING</CTableHeaderCell>
                           </CTableRow>
-                          <CTableRow aria-colspan={4}>
-                            <CTableHeaderCell>Semester: 4 | Batch: B1</CTableHeaderCell>
-                          </CTableRow>
-                          <CTableRow aria-colspan={4}>
-                            <CTableHeaderCell>Subject: AJAVA | DATA: 13/02/2024</CTableHeaderCell>
+                          <CTableRow aria-colspan={5}>
+                            <CTableHeaderCell colSpan={5}>Subject: {session_data.lecture.subject.subject_name} | DATE: {session_data.lecture.session.day}</CTableHeaderCell>
                           </CTableRow>
                           <CTableRow>
                             {/* <CTableHeaderCell>SR. NO</CTableHeaderCell> */}
                             <CTableHeaderCell>Enrollment No</CTableHeaderCell>
                             <CTableHeaderCell>Student Name</CTableHeaderCell>
                             <CTableHeaderCell>IP Addr</CTableHeaderCell>
+                            <CTableHeaderCell>Batch</CTableHeaderCell>
                             <CTableHeaderCell>Status</CTableHeaderCell>
                           </CTableRow>
                         </CTableHead>
@@ -203,22 +218,32 @@ const Sessionmanage = () => {
                                         <div>{item.student.sr_no}</div>   
                                 </CTableDataCell> */}
                                 <CTableDataCell>
-                                  <div>{item.student.enrollment}</div>
+                                  <div>{item.student.enrollment ? item.student.enrollment: "-"}</div>
                                 </CTableDataCell>
                                 <CTableDataCell>
-                                  <div>{item.student.profile.name}</div>
+                                  <div>{item.student.profile.name ? item.student.profile.name: '-'}</div>
                                 </CTableDataCell>
                                 <CTableDataCell>
-                                  <div>{item.marking_ip}</div>
+                                  <div>{item.marking_ip ? item.marking_ip : '-'}</div>
                                 </CTableDataCell>
                                 <CTableDataCell>
+                                <div>
+                                  {item.batches
+                                    ? item.batches.map((batch, index) => (
+                                        <span key={index}>{batch.batch_name.toUpperCase()}</span>
+                                      ))
+                                    : '-'}
+                                </div>
+
+                                </CTableDataCell>
+                                {item.is_present ? (<CTableDataCell>
                                   <div className="text-success">
                                     <svg
                                       xmlns="http://www.w3.org/2000/svg"
                                       width="16"
                                       height="16"
                                       fill="currentColor"
-                                      className="bi bi-patch-check"
+                                      className="mx-auto bi bi-patch-check"
                                       viewBox="0 0 16 16"
                                     >
                                       <path
@@ -227,12 +252,22 @@ const Sessionmanage = () => {
                                       />
                                       <path d="m10.273 2.513-.921-.944.715-.698.622.637.89-.011a2.89 2.89 0 0 1 2.924 2.924l-.01.89.636.622a2.89 2.89 0 0 1 0 4.134l-.637.622.011.89a2.89 2.89 0 0 1-2.924 2.924l-.89-.01-.622.636a2.89 2.89 0 0 1-4.134 0l-.622-.637-.89.011a2.89 2.89 0 0 1-2.924-2.924l.01-.89-.636-.622a2.89 2.89 0 0 1 0-4.134l.637-.622-.011-.89a2.89 2.89 0 0 1 2.924-2.924l.89.01.622-.636a2.89 2.89 0 0 1 4.134 0l-.715.698a1.89 1.89 0 0 0-2.704 0l-.92.944-1.32-.016a1.89 1.89 0 0 0-1.911 1.912l.016 1.318-.944.921a1.89 1.89 0 0 0 0 2.704l.944.92-.016 1.32a1.89 1.89 0 0 0 1.912 1.911l1.318-.016.921.944a1.89 1.89 0 0 0 2.704 0l.92-.944 1.32.016a1.89 1.89 0 0 0 1.911-1.912l-.016-1.318.944-.921a1.89 1.89 0 0 0 0-2.704l-.944-.92.016-1.32a1.89 1.89 0 0 0-1.912-1.911z" />
                                     </svg>
+                                    <p style={{visibility:'hidden'}}>P</p>
                                   </div>
-                                </CTableDataCell>
+                                </CTableDataCell>): (<CTableDataCell>
+                                  <div className="text-danger">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-octagon" viewBox="0 0 16 16">
+                                    <path d="M4.54.146A.5.5 0 0 1 4.893 0h6.214a.5.5 0 0 1 .353.146l4.394 4.394a.5.5 0 0 1 .146.353v6.214a.5.5 0 0 1-.146.353l-4.394 4.394a.5.5 0 0 1-.353.146H4.893a.5.5 0 0 1-.353-.146L.146 11.46A.5.5 0 0 1 0 11.107V4.893a.5.5 0 0 1 .146-.353zM5.1 1 1 5.1v5.8L5.1 15h5.8l4.1-4.1V5.1L10.9 1z"/>
+                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                                  </svg>
+                                    <p style={{visibility:'hidden'}}>F</p>
+                                  </div>
+                                </CTableDataCell>)}
                               </CTableRow>
                             ))}
                         </CTableBody>
-                      </CTable>
+                      </table>
+                      </div>
                     </CCol>
                   </CRow>
                 </CCardBody>
