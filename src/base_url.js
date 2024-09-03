ef5692e1863e537c51f8a8ecc10ad8692e3ffc28
Manav1011 <manavshah1011.ms@gmail.com>
2024-09-03 15:12:01 +0530

    clear

--- src/base_url.js
+++ src/base_url.js
@@ -1,7 +1,7 @@
-export const base_url= `https://smartroll.ldce.mnv-dev.live`;
-export const websocket = `wss://smartroll.ldce.mnv-dev.live`;
-// export const base_url= `http://localhost:8000`;
-// export const websocket = `ws://localhost:8000`;
+// export const base_url= `https://smartroll.ldce.mnv-dev.live`;
+// export const websocket = `wss://smartroll.ldce.mnv-dev.live`;
+export const base_url= `http://localhost:8000`;
+export const websocket = `ws://localhost:8000`;
 
 // const token = localStorage.getItem('accessToken')
 // const header = {
